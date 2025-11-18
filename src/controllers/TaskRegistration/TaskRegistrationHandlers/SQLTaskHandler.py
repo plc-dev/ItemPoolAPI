28c89d3457c2e5ca490459e954e75b76c2967194
plc-dev <33401663+plc-dev@users.noreply.github.com>
2025-11-18 13:52:14 +0100

    feat: add task collections (#5)

--- src/controllers/TaskRegistration/TaskRegistrationHandlers/SQLTaskHandler.py
+++ src/controllers/TaskRegistration/TaskRegistrationHandlers/SQLTaskHandler.py
@@ -3,6 +3,7 @@ from ....models.Task import SQLTask, ResponseStatus, ResponseResult
 
 import logging
 
+
 class SQLTaskHandler(TaskHandler):
     def __init__(self, dao, task_material_controller):
         super().__init__(dao, task_material_controller)
@@ -10,7 +11,9 @@ class SQLTaskHandler(TaskHandler):
     def process_task(self, task: SQLTask):
         try:
             instruction_ids = self._register_materials(task.task_stimulus.instruction)
-            problem_statement_ids = self._register_materials(task.task_stimulus.problem_statement)
+            problem_statement_ids = self._register_materials(
+                task.task_stimulus.problem_statement
+            )
             schema_ids = self._register_materials(task.task_stimulus.db_schema)
             database_ids = self._register_materials(task.task_stimulus.database)
 
@@ -18,27 +21,26 @@ class SQLTaskHandler(TaskHandler):
                 "instruction_ids": instruction_ids,
                 "problem_statement_ids": problem_statement_ids,
                 "schema_ids": schema_ids,
-                "database_ids": database_ids
+                "database_ids": database_ids,
             }
 
             query_ids = self._register_materials(task.task_solutions.query)
 
-            solution_ids = {
-                "query_ids": query_ids
-            }
-            
+            solution_ids = {"query_ids": query_ids}
+
             return {
                 "status": ResponseStatus.success,
                 "stimulus_ids": stimulus_ids,
                 "solution_ids": solution_ids,
-                "result": ResponseResult(message = "Task successfully registered")
+                "metadata": task.metadata,
+                "result": ResponseResult(message="Task successfully registered"),
             }
 
         except Exception as e:
             logging.getLogger("uvicorn.error").error(e)
             return {
                 "status": ResponseStatus.error,
-                "result": ResponseResult(message = str(e)),
+                "result": ResponseResult(message=str(e)),
                 "stimulus_ids": {},
-                "solution_ids": {}
+                "solution_ids": {},
             }
